9768dfde8301ffdef5a5362a2bccc8a5f881bd75
Alfredo Fernandez <afernandez8781@hotmail.com>
2018-12-10 21:11:27 -0400

    Part 30 Testing the comment box

--- resources/assets/js/components/StatusListItem.vue
+++ resources/assets/js/components/StatusListItem.vue
@@ -21,6 +21,14 @@
 				<i class="far fa-thumbs-up"></i>
 				<span dusk="likes-count">{{ status.likes_count }}</span>
 			</div>
+			
+			<form @submit.prevent="addComment">
+				<textarea name="comment" v-model="newComment"></textarea>
+				<button dusk="comment-btn">Enviar</button>
+			</form>
+
+			<div v-for="comment in comments">{{ comment.body }}</div>
+
 		</div>
 	</div>
 </template>
@@ -35,7 +43,22 @@
 				required: true
 			},
 		},
-		components: { LikeBtn }
+		components: { LikeBtn },
+		data() {
+			return {
+				newComment: '',
+				comments: []
+			}
+		},
+		methods: {
+			addComment(){
+				axios.post(`/statuses/${this.status.id}/comments`, {body: this.newComment})
+					.then(res => {
+						this.newComment = '';
+						this.comments.push(res.data.data);
+					})
+			}
+		}
 	}
 </script>
 
