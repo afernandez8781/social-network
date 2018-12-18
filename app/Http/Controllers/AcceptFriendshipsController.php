e5c0f4a5610bded2e35c7ebba02a8955830cc2e5
Alfredo Fernandez <afernandez8781@hotmail.com>
2018-12-17 23:58:17 -0400

    Part 55 Deny request with vue.js

--- app/Http/Controllers/AcceptFriendshipsController.php
+++ app/Http/Controllers/AcceptFriendshipsController.php
@@ -24,6 +24,10 @@ class AcceptFriendshipsController extends Controller
     		'sender_id' => $sender->id,
     		'recipient_id' => auth()->id(),
     	])->update(['status' => 'accepted']);
+
+        return response()->json([
+            'friendship_status' => 'accepted'
+        ]);
     }
 
     public function destroy(User $sender)
@@ -32,5 +36,9 @@ class AcceptFriendshipsController extends Controller
     		'sender_id' => $sender->id,
     		'recipient_id' => auth()->id(),
     	])->update(['status' => 'denied']);
+
+        return response()->json([
+            'friendship_status' => 'denied'
+        ]);
     }
 }
